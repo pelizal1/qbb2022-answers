03c8d8fa1e9e3231f6d159dc580ee55e25b778f9
Paul <pelizal1@jh.edu>
2022-10-14 10:26:51 -0400

    week4

--- week4/gwas_data/week4hw.py
+++ week4/gwas_data/week4hw.py
@@ -49,43 +49,34 @@ df_CB = np.genfromtxt("CB1908_IC50/phenotype_gwas_CB1908_IC50.assoc.linear",
                         names = True)
 
 CB_minuslog10pvalue = -np.log10(df_CB['P'])
-print(CB_minuslog10pvalue)
+
 # print(np.shape(df_CB))
 # print(np.shape(CB_minuslog10pvalue))
 # df_CB_all = np.concatenate((df_CB,CB_minuslog10pvalue), axis = 0)
 
 
-
-# # -log_10(pvalue)
-# df['minuslog10pvalue'] = -np.log10(df.p)
-# df.chromosome = df.chromosome.astype('category')
-# df.chromosome = df.chromosome.cat.set_categories(['ch-%i' % i for i in range(12)], ordered=True)
-# df = df.sort_values('chromosome')
-#
-# # How to plot gene vs. -log10(pvalue) and colour it by chromosome?
+# How to plot gene vs. -log10(pvalue) and colour it by chromosome?
 # df['ind'] = range(len(df))
 # df_grouped = df.groupby(('chromosome'))
-#
-# # manhattan plot
-# fig = plt.figure(figsize=(14, 8)) # Set the figure size
-# ax = fig.add_subplot(111)
-# colors = ['darkred','darkgreen','darkblue', 'gold']
-# x_labels = []
-# x_labels_pos = []
-# for num, (name, group) in enumerate(df_grouped):
-#     group.plot(kind='scatter', x='ind', y='minuslog10pvalue',color=colors[num % len(colors)], ax=ax)
-#     x_labels.append(name)
-#     x_labels_pos.append((group['ind'].iloc[-1] - (group['ind'].iloc[-1] - group['ind'].iloc[0])/2))
-# ax.set_xticks(x_labels_pos)
-# ax.set_xticklabels(x_labels)
-#
-# # set axis limits
-# ax.set_xlim([0, len(df)])
-# ax.set_ylim([0, 3.5])
-#
-# # x axis label
-# ax.set_xlabel('Chromosome')
-#
-# # show the graph
-# plt.show()
+
+# manhattan plot
+fig,ax = plt.subplots()
+x_labels = []
+x_labels_pos = []
+for num, (name, group) in enumerate(df_grouped):
+    group.plot(kind='scatter', x='ind', y='minuslog10pvalue',color=colors[num % len(colors)], ax=ax)
+    x_labels.append(name)
+    x_labels_pos.append((group['ind'].iloc[-1] - (group['ind'].iloc[-1] - group['ind'].iloc[0])/2))
+ax.set_xticks(x_labels_pos)
+ax.set_xticklabels(x_labels)
+
+# set axis limits
+ax.set_xlim([0, len(df)])
+ax.set_ylim([0, 3.5])
+
+# x axis label
+ax.set_xlabel('Chromosome')
+
+# show the graph
+plt.show()
                         
